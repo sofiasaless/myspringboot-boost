206f45564230183c53d63e0d1d1d703dc28efef1
sofiasaless <sofiasaleswk@gmail.com>
2024-12-21 15:25:45 -0300

    feat: salvando entidades válidas no banco de dados

--- src/main/java/br/com/sofiasaless/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/sofiasaless/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -5,17 +5,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.sofiasaless.gestao_vagas.exceptions.UserFoundException;
 import br.com.sofiasaless.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.sofiasaless.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/candidate")
+@RequiredArgsConstructor
 public class CandidateController {
+
+
+    private final CandidateRepository candidateRepository;
   
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getEmail());
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail()).ifPresent((user) -> {
+            throw new UserFoundException(); 
+        });
+        
+        return this.candidateRepository.save(candidateEntity);
+        
     }
 
 }
