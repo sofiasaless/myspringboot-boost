b88c929f2c598fc54d86afb9aebf35e0dcb6716c
sofiasaless <sofiasaleswk@gmail.com>
2024-12-21 00:20:32 -0300

    feat: primeiro handler, este para validação de campos

---
+++ src/main/java/br/com/sofiasaless/gestao_vagas/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,13 @@
+package br.com.sofiasaless.gestao_vagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessageDTO {
+    private String message;
+    private String field;
+}
