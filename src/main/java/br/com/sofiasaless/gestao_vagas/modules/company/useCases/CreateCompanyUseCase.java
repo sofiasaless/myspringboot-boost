692ea8f74c38b73633fa5d74c099d3c738dbe8fd
sofiasaless <sofiasaleswk@gmail.com>
2024-12-21 17:56:22 -0300

    feat: java jwt e criptografando a senha

--- src/main/java/br/com/sofiasaless/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/sofiasaless/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -1,5 +1,6 @@
 package br.com.sofiasaless.gestao_vagas.modules.company.useCases;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.sofiasaless.gestao_vagas.exceptions.UserFoundException;
@@ -13,10 +14,15 @@ public class CreateCompanyUseCase {
     
     private final CompanyRepository companyRepository;
     
+    private final PasswordEncoder passwordEncoder;
+
     public CompanyEntity execute (CompanyEntity companyEntity) {
         this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail()).ifPresent((company) -> {
             throw new UserFoundException();
         });
+
+        var password = passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(password);
 
         return this.companyRepository.save(companyEntity);
     }
