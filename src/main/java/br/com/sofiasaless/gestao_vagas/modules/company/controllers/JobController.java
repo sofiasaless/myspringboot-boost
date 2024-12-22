57c254e595ca5705f74e98546cbc997f7bced30f
sofiasaless <sofiasaleswk@gmail.com>
2024-12-22 05:23:21 -0300

    refactor: job controller com novo requestbody

--- src/main/java/br/com/sofiasaless/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/sofiasaless/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,13 +1,17 @@
 package br.com.sofiasaless.gestao_vagas.modules.company.controllers;
 
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.sofiasaless.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.sofiasaless.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.sofiasaless.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -19,7 +23,18 @@ public class JobController {
     private final CreateJobUseCase createJobUseCase;
 
     @PostMapping("/")
-    public JobEntity create (@Valid @RequestBody JobEntity jobEntity) {
+    public JobEntity create (@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+        // setando o company id que vai vim pelo header do auth, não mais do json
+        var companyId = request.getAttribute("company_id");
+
+        var jobEntity = JobEntity.builder()
+            .description(createJobDTO.getDescription())
+            .companyId(UUID.fromString(companyId.toString()))
+            .benefits(createJobDTO.getBenefits())
+            .level(createJobDTO.getLevel())
+            .build()
+        ;
+
         return this.createJobUseCase.execute(jobEntity);
     }
 
