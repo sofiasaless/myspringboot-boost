b00d76baa01f54fa53f5bea4c9ac4ae41244970a
sofiasaless <sofiasaleswk@gmail.com>
2024-12-23 17:02:07 -0300

    feat: rota de perfil do candidato

--- src/main/java/br/com/sofiasaless/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/sofiasaless/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,15 +1,20 @@
 package br.com.sofiasaless.gestao_vagas.modules.candidate.controllers;
 
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
 
 import br.com.sofiasaless.gestao_vagas.exceptions.UserFoundException;
 import br.com.sofiasaless.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.sofiasaless.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.sofiasaless.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.sofiasaless.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -19,12 +24,27 @@ import lombok.RequiredArgsConstructor;
 public class CandidateController {
 
     private final CreateCandidateUseCase createCandidateUseCase;
+
+    private final ProfileCandidateUseCase profileCandidateUseCase;
   
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
         try {
             var result = this.createCandidateUseCase.execute(candidateEntity);
             return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    // rota para retornar as informações de perfil do candidato
+    @GetExchange("/profile")
+    public ResponseEntity<Object> get(HttpServletRequest request) {
+        var idCandidate = request.getAttribute("candidate_id");
+
+        try {
+            var profile = this.profileCandidateUseCase.execute(UUID.fromString(idCandidate.toString()));
+            return ResponseEntity.ok().body(profile);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
