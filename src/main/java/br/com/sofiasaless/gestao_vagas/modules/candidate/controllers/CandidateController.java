0e8d4e67ed04a818d077fa220a0811684c4ed4c0
sofiasaless <sofiasaleswk@gmail.com>
2024-12-26 01:05:06 -0300

    feat: adicionando swagger no candidate controller

--- src/main/java/br/com/sofiasaless/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/sofiasaless/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.sofiasaless.gestao_vagas.modules.candidate.controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
@@ -8,13 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.sofiasaless.gestao_vagas.exceptions.UserFoundException;
 import br.com.sofiasaless.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.sofiasaless.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.sofiasaless.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.sofiasaless.gestao_vagas.modules.candidate.useCases.ListAllJobsByFIlterUseCase;
 import br.com.sofiasaless.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import br.com.sofiasaless.gestao_vagas.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +38,8 @@ public class CandidateController {
     private final CreateCandidateUseCase createCandidateUseCase;
 
     private final ProfileCandidateUseCase profileCandidateUseCase;
+
+    private final ListAllJobsByFIlterUseCase listAllJobsByFIlterUseCase;
   
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -50,6 +63,24 @@ public class CandidateController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/job")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Informações do candidato")
+    @Operation(
+        summary = "Listagem de vagas disponíveis para o candidato", 
+        description = "Essa função é reponsável por listar todas as vagas disponíveis baseada no filtro"
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(
+                array = @ArraySchema(schema = @Schema(implementation = JobEntity.class))
+            )
+        })
+    })
+    public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+        return this.listAllJobsByFIlterUseCase.execute(filter);
     }
 
 }
